30a3b40b4df604562777942c685aac362995d0bf
wilson-pc <wilverhidalgo@outlook.com>
2021-04-04 11:23:46 -0400

    add limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: '100mb' }));
   await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
